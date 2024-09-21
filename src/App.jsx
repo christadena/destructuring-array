f6cae4e510296731bf9b007468eea7da7cdf5817
Chris Gerald Tadena <sterbenkazuto17@gmail.com>
2024-09-21 22:32:34 +0800

    updated format and placement of tbody

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import cars from './Cars'
-import './App.css'
-
-
+import cars from "./Cars";
+import "./App.css";
 
 function App() {
   const carData = cars.map((car) => {
@@ -11,29 +9,31 @@ function App() {
       topColour: car.coloursByPopularity[0],
     };
   });
-  
+
   return (
     <>
       <div>
-      <table>
-        <tr>
-          <th>Brand</th>
-          <th>Top Speed</th>
-          <th>Top color</th>
-        </tr>
-        <tbody>
-          {carData.map((car, index) => (
-            <tr key={index}>
-              <td>{car.model}</td>
-              <td>{car.topSpeed}</td>
-              <td>{car.topColour}</td>
+        <table>
+          <thead>
+            <tr>
+              <th>Brand</th>
+              <th>Top Speed</th>
+              <th>Top Colour</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {carData.map((car, index) => (
+              <tr key={index}>
+                <td>{car.model}</td>
+                <td>{car.topSpeed}</td>
+                <td>{car.topColour}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
